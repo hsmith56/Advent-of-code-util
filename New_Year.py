448aa13e2d8bd9709cef37155f51714de765b5b5
Harrison <47962432+hsmith56@users.noreply.github.com>
2021-12-02 21:09:25 +0000

    changed print statements

--- New_Year.py
+++ New_Year.py
@@ -13,11 +13,10 @@ for i in range(1, 26):
     try:
         with open(cwd + f"day{i}.py", 'w') as f:
             f.write(
-                f"import random, math\n\ndef load():\n\twith open('day{i}Input.txt') as file:\n\t\tdata = file.readlines()\n\t\td = [line.strip() for line in data]\n\treturn d\n\ninpt = load()\ndef part1(inpt):\n\tinpt = ''.join(inpt)\n\tinpt = [n for n in inpt.split(',')]\n\treturn 0\n\ndef part2(inpt):\n\treturn 0\n\nprint(inpt)"
+                f"import random, math\n\ndef load():\n\twith open('day{i}Input.txt') as file:\n\t\tdata = file.readlines()\n\t\td = [line.strip() for line in data]\n\treturn d\n\ninpt = load()\ndef part1(inpt):\n\treturn 0\n\ndef part2(inpt):\n\treturn 0\n\nprint(part1(inpt), part2(inpt))"
             )
             f.close()
         immediately_close = open(cwd + f"day{i}Input.txt", 'w')
         immediately_close.close()
     except FileExistsError as e:
         print(e)
-
