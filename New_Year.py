34affde40f6ab7236da0420364aab99e10b926da
Harrison <47962432+hsmith56@users.noreply.github.com>
2021-12-22 15:54:10 +0000

    better directory setup, better names

--- New_Year.py
+++ New_Year.py
@@ -1,5 +1,4 @@
 import os
-
 year = input('What year would you like to work on: ')
 
 try:
@@ -9,14 +8,16 @@ except FileExistsError as e:
 
 cwd = os.getcwd() + '\\' + year + '\\'
 
+
 for i in range(1, 26):
     try:
-        with open(cwd + f"day{i}.py", 'w') as f:
-            f.write(
-                f"import random, math\n\ndef load():\n\twith open('day{i}Input.txt') as file:\n\t\tdata = file.readlines()\n\t\td = [line.strip() for line in data]\n\treturn d\n\ninpt = load()\ndef part1(inpt):\n\treturn 0\n\ndef part2(inpt):\n\treturn 0\n\nprint(f'Part1: {part1(inpt)}\nPart2: {part2(inpt)}')"
+        os.makedirs(os.path.join(cwd, f"day{i:02d}"))
+        with open(os.path.join(cwd, f"day{i:02d}" , f'day{i:02d}.py'), 'w') as file:
+            file.write(
+                f"import random, math\n\ndef load():\n\twith open('{year}/day{i:02d}/day{i:02d}Input.txt') as file:\n\t\tdata = file.readlines()\n\t\td = [line.strip() for line in data]\n\treturn d\n\ninpt = load()\ndef part1(inpt):\n\treturn inpt\n\ndef part2(inpt):\n\treturn 0\n\nprint(f'Part1: " + "{part1(inpt)}\\nPart2: {part2(inpt)}')"
             )
-            f.close()
-        immediately_close = open(cwd + f"day{i}Input.txt", 'w')
+            file.close()
+        immediately_close = open(os.path.join(cwd, f"day{i:02d}", f"day{i:02d}Input.txt"), 'w')
         immediately_close.close()
     except FileExistsError as e:
         print(e)
