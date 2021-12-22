d8ccc1423f5a15ac5f412c1517f1e838448da9cd
Harrison <47962432+hsmith56@users.noreply.github.com>
2021-12-22 16:04:46 +0000

    Update New_Year.py

--- New_Year.py
+++ New_Year.py
@@ -13,7 +13,7 @@ for i in range(1, 26):
         os.makedirs(os.path.join(cwd, f"day{i:02d}"))
         with open(os.path.join(cwd, f"day{i:02d}" , f'day{i:02d}.py'), 'w') as file:
             file.write(
-                f"import random, math\n\ndef load():\n\twith open('{year}/day{i:02d}/day{i:02d}Input.txt') as file:\n\t\tdata = file.readlines()\n\t\td = [line.strip() for line in data]\n\treturn d\n\ninpt = load()\ndef part1(inpt):\n\treturn inpt\n\ndef part2(inpt):\n\treturn 0\n\nprint(f'Part1: " + "{part1(inpt)}\\nPart2: {part2(inpt)}')"
+                f"import random, math\n\ndef load():\n\twith open('day{i:02d}/day{i:02d}Input.txt') as file:\n\t\tdata = file.readlines()\n\t\td = [line.strip() for line in data]\n\treturn d\n\ninpt = load()\ndef part1(inpt):\n\treturn inpt\n\ndef part2(inpt):\n\treturn 0\n\nprint(f'Part1: " + "{part1(inpt)}\\nPart2: {part2(inpt)}')"
             )
             file.close()
         immediately_close = open(os.path.join(cwd, f"day{i:02d}", f"day{i:02d}Input.txt"), 'w')
