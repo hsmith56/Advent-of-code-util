8d5cd794fb67666d5be492d7cb8a698bfc8940cf
Harrison <47962432+hsmith56@users.noreply.github.com>
2022-03-24 12:13:13 +0000

    Add example file

--- New_Year.py
+++ New_Year.py
@@ -16,7 +16,10 @@ for i in range(1, 26):
                 f"import random, math\n\ndef load():\n\twith open('day{i:02d}/day{i:02d}Input.txt') as file:\n\t\tdata = file.readlines()\n\t\td = [line.strip() for line in data]\n\treturn d\n\ninpt = load()\ndef part1(inpt):\n\treturn inpt\n\ndef part2(inpt):\n\treturn 0\n\nprint(f'Part1: " + "{part1(inpt)}\\nPart2: {part2(inpt)}')"
             )
             file.close()
-        immediately_close = open(os.path.join(cwd, f"day{i:02d}", f"day{i:02d}Input.txt"), 'w')
-        immediately_close.close()
+        with open(os.path.join(cwd, f"day{i:02d}", f"day{i:02d}Input.txt"), 'w'):
+            pass
+        with open(os.path.join(cwd, f"day{i:02d}", f"day{i:02d}Example.txt"), 'w'):
+            pass
+
     except FileExistsError as e:
         print(e)
